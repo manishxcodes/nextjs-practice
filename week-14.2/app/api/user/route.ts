1056985d321caded619aaef3ff84ccd0b31cf3cc
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-02-13 00:00:25 +0530

    add signup route

--- week-14.2/app/api/user/route.ts
+++ week-14.2/app/api/user/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const client = new PrismaClient();
+import client from "@/db"
 
 export function GET() {
     // database logic
@@ -16,18 +14,28 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
 
     // store the body in db
-    const user = await client.user.create({
-        data: {
-            firstName: body.firstName,
-            lastName: body.lastName,
-            email: body.email,
-            password: body.password
-        }
-    })
+    try {
+        const user = await client.user.create({
+            data: {
+                firstName: body.firstName,
+                lastName: body.lastName,
+                email: body.email,
+                password: body.password
+            }
+        });
+        // header 
+        // console.log(req.headers.get("Authorization"));
 
-    console.log(user.id);
+        // // query parameters
+        // console.log(req.nextUrl.searchParams.get("name"));
 
-    return NextResponse.json({
-        message: "Signup Successful"
-    });
+        return NextResponse.json({
+            message: "Signup Successful"
+        });    
+    }   catch(err: any) {
+            console.log("error:", err);
+            return NextResponse.json({
+                message: "Error while signing up", error: err.message
+            }, {status: 500});
+    } 
 }
