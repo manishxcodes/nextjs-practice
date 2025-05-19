f15599fbcc164918abcdef23d490f1aa9f828097
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-20 02:04:17 +0530

    integrated NEXT_AUTH in getserversession

--- next-auth/app/user/page.tsx
+++ next-auth/app/user/page.tsx
@@ -1,8 +1,9 @@
 import { Appbar } from "@/components/appbar";
 import { getServerSession } from "next-auth"
+import { NEXT_AUTH } from "../lib/auth";
 
 export default async function() {
-    const session = await getServerSession();
+    const session = await getServerSession(NEXT_AUTH);
     return (
         <div>
             <Appbar />
